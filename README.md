221cfb877cd2307864dd67c7490e3cbdc3944516
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-10-03 18:40:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# shopware6BestandsUpdate
+
+Updated den Bestand von Shopware 6 Artikeln ausgehend von einer .csv-Datei
+
+Ausgehend von diesem Forumsbeitrag im Shopware 6 Forum: https://forum.shopware.com/t/lagerbestand-csv-import/68521
+
+**Das Problem:**
+
+Updaten des Bestandes von Artikeln in Shopware 6 setzt zwingend voraus dass man die ProductID ermittelt um dann mit dem Datei-Import arbeiten zu können.
+
+Die .csv-Datei muss wie folgt aufgebaut sein:
+(Trennzeichen: ",")
+
+| ARTIKELNUMMER | BESTAND |
+|---------------|---------|
+| ART0815 | 12 |
+| GUMMIENTE | 14 |
+| WASSER_1_l | 23 |
+
+**ACHTUNG: Die .csv-Datei darf KEINE Überschrift beinhalten - einfach nur Artikelnummer und Bestand in jeder Zeile!**
+
+**Beispieldatei:**
+```
+ART0815,12
+GUMMIENTE,14
+WASSER_1_l,23
+```
+[artikel.csv](artikel.csv)
+
+Der Aufruf erfolgt nun wie folgt:
+```
+shopwareUpdater --url=https://www.meinshop.de --username=admin --passwort=geheim --datei=artikel.csv
+```
+
+Eine entsprechende .exe-Datei kann man unter [Releases](https://github.com/ThomasChr/shopware6BestandsUpdate/releases) runterladen
---
+++ util.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import datetime
+
+myPid = os.getpid()
+
+
+def printLog(logtext):
+    print(datetime.datetime.now().astimezone().isoformat() + " (" + str(myPid) + ") => " + logtext, flush=True)
+
+
+def getShopwareToken(url, username, passwort):
+    headers = {"Accept": "application/json", "Content-Type": "application/json"}
+    payload = {"client_id": "administration", "grant_type": "password", "scopes": "write", "username": username, "password": passwort}
+    r = requests.post(url + "/api/oauth/token", headers=headers, json=payload).json()
+    return r["access_token"]
+
+
+def getShopwareArticleId(url, shopwareToken, articleNo):
+    shopwareID = ""
+    headers = {"Accept": "application/json", "Authorization": "Bearer " + shopwareToken}
+    payload = {"filter": [{"type": "equals", "field": "productNumber", "value": articleNo}], "includes": {}}
+    payload["includes"]["product"] = ["id"]
+    r = requests.post(url + "/api/search/product", json=payload, headers=headers).json()
+    if 'errors' in r:
+        raise ValueError(r)
+    if 'data' in r and len(r['data']) == 1:
+        shopwareID = r['data'][0]['id']
+    return shopwareID
+
+
+def updateArticleStock(url, shopwareToken, productID, stock):
+    headers = {"Accept": "application/json", "Authorization": "Bearer " + shopwareToken}
+    payload = {"stock": int(stock)}
+    requests.patch(url + f"/api/product/{productID}", json=payload, headers=headers)
